c8e84a4840a2f4aee9d142d46f1f02e3f02518d3
suraj0902 <63006370+suraj0902@users.noreply.github.com>
2022-03-15 07:38:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- webpage title -->
+    <title>448 Anakal Suraj / Home</title>
+    <!-- external CSS -->
+    <!-- <link rel="stylesheet" href="./src/style/sass/style.css"> -->
+    <link rel="stylesheet" href="./src/style/sass/style.css?v=<?php echo time(); ?>">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="./public/favicon.ico" type="image/x-icon">
+    <!-- unicons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+<body>
+    <!-- ========= HEADER START ========= -->
+    <header id="header-section">
+      <nav id="header-navbar">
+        <div class="nav-center">
+          <!-- nav header / branding & toggler -->
+          <div class="nav-header">
+            <!-- brand title -->
+            <a href="./index.php">
+              <h1 class="brand-title">Suraj Anakal</h1>
+            </a>
+
+            <!-- navbar toggle button -->
+            <button class="nav-toggle">
+              <i class="uil uil-bars"></i>
+            </button>
+          </div>
+
+          <!-- navbar links -->
+          <ul class="links">
+            <li><a href="./index.php" class="active-link">Home</a></li>
+            <li><a href="./src/pages/trending.php">Trending</a></li>
+            <li><a href="./src/pages/explore.php">Explore Us</a></li>
+            <li><a href="./src/pages/profile.php">Your Profile</a></li>
+            <li><a href="./src/pages/registration.php">Registration</a></li>
+            <li><a href="./src/pages/login.php">Login</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <!-- ========= HEADER END ========= -->
+
+    <!-- ========= MAIN / BODY START ========= -->
+    <!-- ========= MAIN / BODY START ========= -->
+
+    <!-- ========= FOOTER START ========= -->
+    <!-- ========= FOOTER END ========= -->
+
+    <!-- script / js -->
+    <script src="./src/js/responsive-navbar.js"></script>
+</body>
+</html>
